0679c91f3f2df1d3fc2adb25c3e643a389b3dd1b
jshargrave <happyguyhere@hotmail.com>
2016-10-30 13:05:26 -0500

    added readme and modified Road_Network.java

--- src/Road_Network.java
+++ src/Road_Network.java
@@ -3,8 +3,8 @@ public class Road_Network {
 	Segment S; //points to an array of segments
 	GPS_Data G; //points to 
 	
-	public Road_Network(){
-		S = new Segment(); //build road network
+	public Road_Network(){ //build road network
+		S = new Segment(); 
 		
 		
 		

71ed66d2ee09a2f65537e3fb3c1f266d67b47664
jshargrave <happyguyhere@hotmail.com>
2016-11-03 16:42:25 -0500

    succesfull insert to database

--- src/DBMS.java
+++ src/DBMS.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBMS {
-	public Connection startConnection(){
-	    Connection conn = null;
-		Statement statement = null;
+	public void connect(){
+		Connection conn = null;
+		Statement stmt = null;
+		
 		String url = "jdbc:mysql://localhost:3306/";
 		String db = "Ride-Sharing";
 		String user = "root";
@@ -18,7 +19,7 @@ public class DBMS {
 	        Class.forName("com.mysql.jdbc.Driver");
 	        conn = DriverManager.getConnection(url + db, user, pass);
 	        System.out.println("Connection Successful");
-	
+	        stmt = conn.createStatement();
 	    }
 	    catch(ClassNotFoundException error){
 	        System.out.println("Error:" + error.getMessage()); 
@@ -36,18 +37,69 @@ public class DBMS {
 	
 	        }
 	
-	        if (statement != null) 
+	        if (stmt != null) 
 	            try {
-	                statement.close();
+	            	stmt.close();
 	            }
 	
 	        catch(SQLException ignore){
 	
 	        }
 	    }
-	    return conn;
 	}
 	
+	public void insert() {
+		// JDBC driver name and database URL
+		   String DB_URL = "jdbc:mysql://localhost:3306/Ride-Sharing";
+
+		   //  Database credentials
+		   String USER = "root";
+		   String PASS = "";
+
+		   Connection conn = null;
+		   Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to a selected database...");
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      System.out.println("Connected database successfully...");
+		      
+		      //STEP 4: Execute a query
+		      System.out.println("Inserting records into the table...");
+		      stmt = conn.createStatement();
+		      
+		      String sql = "INSERT INTO segments " +
+		                   "VALUES (101, '5', '7')";
+		      stmt.executeUpdate(sql);
+	
+		      System.out.println("Inserted records into the table...");
+
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   System.out.println("Goodbye!");
+		   
+	}
 
 	public void delete(){
 		
--- src/Ridesharing.java
+++ src/Ridesharing.java
@@ -1,7 +1,7 @@
 public class Ridesharing {
 	public static void main(String args[]){
 		DBMS database = new DBMS();
-		database.startConnection();
+		database.insert();
 	
 	}
 }
